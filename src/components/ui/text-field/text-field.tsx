8a8e7c4061b878b440b450cd3b0afe15437cc715
Evgeniy Ivanov <p8635241@gmail.com>
2023-12-14 22:36:32 +0500

    new component textfield

---
+++ src/components/ui/text-field/text-field.tsx
@@ -0,0 +1,12 @@
+import { EyeOutline } from '@/assets'
+
+type TextFieldType = {}
+
+export const TextField = () => {
+  return (
+    <div className={'container'}>
+      <input />
+      <EyeOutline />
+    </div>
+  )
+}
