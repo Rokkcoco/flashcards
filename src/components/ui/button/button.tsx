84ae89df7d3e767b08a1f5977a91dd7312317c50
Evgeniy Ivanov <p8635241@gmail.com>
2023-12-15 00:30:13 +0500

    styles for textfield

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,5 +1,7 @@
 import { ComponentPropsWithoutRef, ElementType } from 'react'
 
+import { Typography } from '@/components/ui/typography'
+
 import s from './button.module.scss'
 
 export type ButtonProps<T extends ElementType = 'button'> = {
@@ -12,9 +14,18 @@ export type ButtonProps<T extends ElementType = 'button'> = {
 export const Button = <T extends ElementType = 'button'>(
   props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
 ) => {
-  const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
+  const {
+    as: Component = 'button',
+    children,
+    className,
+    fullWidth,
+    variant = 'primary',
+    ...rest
+  } = props
 
   return (
-    <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest} />
+    <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest}>
+      <Typography variant={'subtitle_1'}>{children}</Typography>
+    </Component>
   )
 }
--- src/components/ui/text-field/text-field.tsx
+++ src/components/ui/text-field/text-field.tsx
@@ -1,10 +1,58 @@
-import { EyeOutline } from '@/assets'
+import { ChangeEvent, useState } from 'react'
 
+import { EyeOutline } from '@/assets'
+import { Typography } from '@/components/ui/typography'
+import * as Label from '@radix-ui/react-label'
+
+import s from './text-field.module.scss'
 export const TextField = () => {
+  const calls = () => console.log('yes')
+  const label = 'Error!'
+  const [text, setText] = useState('')
+  const changeInputValue = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.currentTarget.value)
+  }
+
   return (
-    <div className={'container'}>
-      <input />
-      <EyeOutline />
+    <div className={s.container}>
+      <input id={'input'} onChange={changeInputValue} placeholder={'Input'} value={text} />
+      <button onClick={calls}>
+        <EyeOutline />
+      </button>
+
+      <Label.Root className={s.label} htmlFor={'input'}>
+        <Typography variant={'caption'}>{label}</Typography>
+      </Label.Root>
     </div>
   )
 }
+
+// Для того, чтобы ваши поля не были стандартного цвета, при выборе из списка ранее вводимых значений, нужно добавить:
+//     input:-webkit-autofill,
+//         input:-webkit-autofill:hover,
+//     input:-webkit-autofill:focus
+// input:-webkit-autofill,
+//     textarea:-webkit-autofill,
+//     textarea:-webkit-autofill:hover
+// textarea:-webkit-autofill:focus,
+//     select:-webkit-autofill,
+//     select:-webkit-autofill:hover,
+//     select:-webkit-autofill:focus {
+//     border: 0;
+//     -webkit-text-fill-color: #1f1f20;
+//     -webkit-box-shadow: 0 0 0 1000px transparent inset;
+//     transition: background-color 5000s ease-in-out 0s;
+//     background: -webkit-linear-gradient(top,  rgba(255,255,255,0) 0%,rgba(0,174,255,0.04) 50%,rgba(255,255,255,0) 51%,rgba(0,174,255,0.03) 100%);
+// }
+// где вместо transparent можно указать любой цвет.
+
+// Ещё для input типа search чтоб не был дефолтный крестик:
+//     input[type='search'] {
+// &::-webkit-search-decoration,
+// &::-webkit-search-cancel-button,
+// &::-webkit-search-results-button,
+// &::-webkit-search-results-decoration {
+//         appearance: none;
+//     }
+// }
+// и если нужно предотвратить предложение раннее введённых значений в конкретном инпуте, ему можно указать пропс autoComplete={'off'}
