e424c2f5b475f5d8179088ffbccad85865fd8185
Evgeniy Ivanov <p8635241@gmail.com>
2023-12-17 03:24:07 +0500

    ref for button component

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef, ElementType } from 'react'
+import { ComponentPropsWithoutRef, ElementType, LegacyRef, forwardRef } from 'react'
 
 import s from './button.module.scss'
 
@@ -9,12 +9,19 @@ export type ButtonProps<T extends ElementType = 'button'> = {
   variant?: 'link' | 'primary' | 'secondary' | 'tertiary'
 } & ComponentPropsWithoutRef<T>
 
-export const Button = <T extends ElementType = 'button'>(
-  props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
-) => {
-  const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
+export const Button = forwardRef(
+  <T extends ElementType = 'button'>(
+    props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>,
+    ref: LegacyRef<T extends ElementType<infer U> ? U : never>
+  ) => {
+    const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
 
-  return (
-    <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest} />
-  )
-}
+    return (
+      <Component
+        className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
+        ref={ref}
+        {...rest}
+      />
+    )
+  }
+)
