83e9fd67286812ae6705409e040eb522a66449e9
Evgeniy Ivanov <p8635241@gmail.com>
2024-01-17 00:56:10 +0500

    fixing styles for drop down component, new story fix

--- src/components/ui/drop-down-menu/drop-down-menu.stories.tsx
+++ src/components/ui/drop-down-menu/drop-down-menu.stories.tsx
@@ -1,4 +1,4 @@
-import { CloseOutline } from '@/assets'
+import { CloseOutline, Edit2Outline, PlayCircleOutline, TrashOutline } from '@/assets'
 import { Avatar } from '@/components/ui/avatar'
 import { Meta, StoryObj } from '@storybook/react'
 
@@ -15,23 +15,59 @@ type Story = StoryObj<typeof meta>
 
 export const DropdownMenuStory: Story = {
   args: {
-    trigger: 'YES',
+    trigger: (
+      <button>
+        <Avatar>John</Avatar>
+      </button>
+    ),
   },
   render: args => {
     return (
-      <DropdownMenu {...args}>
-        <DropdownItem>
-          YO <CloseOutline />
-        </DropdownItem>
-        <DropdownSeparator />
-        <DropdownItem>
-          COOL <CloseOutline />
-        </DropdownItem>
-        <DropdownSeparator />
-        <DropdownItem>
-          <Avatar>DG</Avatar>
-        </DropdownItem>
-      </DropdownMenu>
+      <div style={{ marginLeft: '150px' }}>
+        <DropdownMenu {...args}>
+          <DropdownItem>
+            YOooooooo <CloseOutline />
+          </DropdownItem>
+          <DropdownSeparator />
+          <DropdownItem>
+            COOLOOOOoooooo <CloseOutline />
+          </DropdownItem>
+          <DropdownSeparator />
+          <DropdownItem>
+            <Avatar>DG</Avatar>
+          </DropdownItem>
+        </DropdownMenu>
+      </div>
+    )
+  },
+}
+export const DropdownMenuWithoutTriggerStory: Story = {
+  args: {
+    trigger: (
+      <button>
+        <Avatar>John</Avatar>
+      </button>
+    ),
+  },
+
+  render: args => {
+    return (
+      <div style={{ marginLeft: '150px' }}>
+        <DropdownMenu {...args}>
+          <DropdownItem>
+            <PlayCircleOutline /> Learn
+          </DropdownItem>
+          <DropdownSeparator />
+          <DropdownItem>
+            <Edit2Outline /> Edit
+          </DropdownItem>
+          <DropdownSeparator />
+          <DropdownItem>
+            <TrashOutline />
+            Delete
+          </DropdownItem>
+        </DropdownMenu>
+      </div>
     )
   },
 }
--- src/components/ui/drop-down-menu/drop-down-menu.tsx
+++ src/components/ui/drop-down-menu/drop-down-menu.tsx
@@ -1,12 +1,14 @@
 import { ComponentPropsWithoutRef, ElementRef, ReactNode, forwardRef } from 'react'
 
-import { Avatar } from '@/components/ui/avatar'
 import * as DropdownMenuRadix from '@radix-ui/react-dropdown-menu'
 import { clsx } from 'clsx'
 
 import s from './drop-down-menu.module.scss'
 
-type DropdownItemProps = {} & ComponentPropsWithoutRef<typeof DropdownMenuRadix.Item>
+type DropdownItemProps = {} & Omit<
+  ComponentPropsWithoutRef<typeof DropdownMenuRadix.Item>,
+  'asChild'
+>
 export const DropdownItem = forwardRef<
   ElementRef<typeof DropdownMenuRadix.Item>,
   DropdownItemProps
@@ -39,36 +41,33 @@ export const DropdownSeparator = forwardRef<
 })
 
 type Props = {
-  alt?: string
-  src?: string
+  align?: 'center' | 'end' | 'start'
   trigger: ReactNode
-} & ComponentPropsWithoutRef<typeof DropdownMenuRadix.Root> &
-  ComponentPropsWithoutRef<'div'>
+} & ComponentPropsWithoutRef<typeof DropdownMenuRadix.Content>
 export const DropdownMenu = forwardRef<ElementRef<typeof DropdownMenuRadix.Content>, Props>(
   (props, ref) => {
-    const { alt, children, className, src, trigger, ...rest } = props
+    const { align = 'end', children, className, trigger, ...rest } = props
 
     const classNames = {
+      arrow: s.arrow,
       content: clsx(s.content, className),
+      trigger: s.trigger,
     }
 
-    // items: { content: ReactNode }[]
-    /*  if (isValidElement(t.content) && t.content.props !== undefined) {
-          if (t.content.props.children[index].type.__docgenInfo.displayName === 'Avatar') {
-              console.log('yes')
-          }
-      }*/
     return (
       <DropdownMenuRadix.Root {...rest}>
-        <DropdownMenuRadix.Trigger>
-          <Avatar alt={alt} src={src}>
-            {trigger}
-          </Avatar>
+        <DropdownMenuRadix.Trigger asChild className={classNames.trigger}>
+          {trigger}
         </DropdownMenuRadix.Trigger>
         <DropdownMenuRadix.Portal>
-          <DropdownMenuRadix.Content className={classNames.content} ref={ref}>
+          <DropdownMenuRadix.Content
+            className={classNames.content}
+            {...rest}
+            align={align}
+            ref={ref}
+          >
             {children}
-            <DropdownMenuRadix.Arrow className={s.arrow} />
+            <DropdownMenuRadix.Arrow className={classNames.arrow} />
           </DropdownMenuRadix.Content>
         </DropdownMenuRadix.Portal>
       </DropdownMenuRadix.Root>
