ffc2d3ad52c0928bb5f7ebb96cbff860cd29d56b
Егор Серафонтов <pixaretyiypop@gmail.com>
2024-01-20 19:16:24 +0300

    refactor select stories

--- src/components/ui/select/select.stories.tsx
+++ src/components/ui/select/select.stories.tsx
@@ -15,8 +15,8 @@ type Story = StoryObj<typeof meta>
 
 export const SelectStory: Story = {
   args: {
+    label: 'Select one answer',
     options: { audio: 'Audio', picture: 'Picture', video: 'Video' },
-    title: 'Select one answer',
     value: 'picture',
   },
   render: args => {
@@ -28,8 +28,8 @@ export const SelectStory: Story = {
 export const Disabled: Story = {
   args: {
     disabled: true,
+    label: 'Select one answer',
     options: { audio: 'Audio', picture: 'Picture', video: 'Video' },
-    title: 'Select one answer',
     value: 'picture',
   },
   render: args => {
