215ff09c4e9eeed3006f5bd8301a709b834d1d35
Evgeniy Ivanov <p8635241@gmail.com>
2024-02-20 18:58:44 +0500

    new function for as prop predict

--- src/components/ui/typography/typography.tsx
+++ src/components/ui/typography/typography.tsx
@@ -37,7 +37,38 @@ export const Typography: TypographyComponent = forwardRef(
   <T extends ElementType = 'p'>(props: TypographyProps<T>, ref: PolymorphicRef<T>) => {
     const { as, children, className, text = '', variant = 'body_1', ...rest } = props
 
-    const Component = as || 'p'
+    function exhaustiveCheck(value: never) {
+      return value
+    }
+    //todo review this function
+    function typographyVar(as: ElementType = 'p', variant: Props<T>['variant'] = 'body_1') {
+      switch (variant) {
+        case 'body_1':
+        case 'body_2':
+        case 'caption':
+        case 'subtitle_1':
+        case 'subtitle_2':
+        case 'overline':
+        case 'link_1':
+        case 'link_2':
+        case 'large':
+        case 'error':
+          return { as }
+        case 'label':
+          return { as: 'label' }
+        case 'h1':
+          return { as: 'h1' }
+        case 'h2':
+          return { as: 'h2' }
+        case 'h3':
+          return { as: 'h3' }
+        default:
+          return exhaustiveCheck(variant)
+      }
+    }
+
+    // const Component = as || 'p'
+    const Component = typographyVar(as, variant).as
 
     return (
       <Component className={`${s.typography} ${s[variant]} ${className}`} ref={ref} {...rest}>
